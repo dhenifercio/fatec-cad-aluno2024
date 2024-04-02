8e07d05633c1ca75e59d1979fcddb414f80be893
Dhenifer Cioffi <dhenifercioffiflora@gmail.com>
2024-04-02 12:05:17 -0300

    services and json-server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Student } from './student';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cad-aluno';
+
+ 
 }
---
+++ src/app/students/students.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from '../student.service';
+import { Student } from '../student';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-students',
+  templateUrl: './students.component.html',
+  styleUrl: './students.component.css'
+})
+export class StudentsComponent implements OnInit{
+  students: Student[]=[];
+
+  formGroupStudent : FormGroup;
+  isEditing: boolean=false;
+  
+  constructor(private formBuilder: FormBuilder,
+    private service: StudentService){
+  
+    this.formGroupStudent = formBuilder.group ({
+      id: [''],
+      name: [''],
+      course: ['']
+   });
+  }
+  ngOnInit(): void {
+    this.loadStudents();
+  }
+
+  loadStudents (){
+    this.service.getStudents().subscribe({
+      next: data => this.students=data
+
+    });
+  }
+  
+   save(){
+    if(this.isEditing){
+      this.service.update(this.formGroupStudent.value).subscribe({
+        next: () => {
+          this.loadStudents();
+          this.isEditing = false
+        }
+      })
+    }
+    else{
+      this.service.save(this.formGroupStudent.value).subscribe({
+        next: data => this.students.push(data)
+      });
+    }
+    this.formGroupStudent.reset()
+      
+}
+
+delete(student:Student){
+  this.service.delete(student).subscribe({
+    next: () => this.loadStudents()
+  })
+}
+
+edit (student:Student){
+  this.formGroupStudent.setValue(student);
+  this.isEditing=true;
+}
+}
